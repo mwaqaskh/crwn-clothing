6f4d50341b29da5a1087bc9d147f4903bb7fb9f1
mwaqaskh <mwaqaskh@gmail.com>
2020-06-30 19:56:16 +1000

    added directory and menu-item components

---
+++ src/components/directory/diretory.components.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import "./directory.styles.scss";
+import sections from "./directory.data";
+import MenuItem from "../menu-item/menu-item.component";
+
+class Directory extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      sections: sections,
+    };
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map(({ id, title }) => (
+          <MenuItem key={id} title={title} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Directory;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "./homepage.styles.scss";
 
+import Directory from "../../components/directory/diretory.components";
+
 const HomePage = () => (
   <div className="homepage">
     <h1>Home Page</h1>
+    <Directory />
   </div>
 );
 
